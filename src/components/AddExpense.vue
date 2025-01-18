550d892864cefc1007a76362d1d52c94803aff63
mohammedbalsaniya <mohammedbalsaniya111@gmail.com>
2025-01-18 17:05:50 +0530

    Add script file on AddExpense to take value from to user

--- src/components/AddExpense.vue
+++ src/components/AddExpense.vue
@@ -1,13 +1,28 @@
+<script setup>
+ import { reactive , ref} from 'vue';
+ const amount = ref('');
+ const date = ref('');
+ const selected = ref('');
+ const description = ref('');
+
+
+
+const addExp =  () =>{
+  console.log(amount.value, date.value, selected.value, description.value)
+}
+
+</script>
 <template>
   <div class="container mt-5 add-expense">
     <h1 class="text-center mb-4">Add New Expense</h1>
 
-    <form @submit.prevent="" class="needs-validation" novalidate>
+    <form @submit.prevent="addExp" class="needs-validation" novalidate>
       <!-- Amount Input -->
       <div class="mb-3">
         <label for="amount" class="form-label">Amount</label>
         <input
           type="number"
+          v-model="amount"
           id="amount"
           class="form-control"
           required
@@ -19,7 +34,7 @@
       <!-- Category Dropdown -->
       <div class="mb-3">
         <label for="category" class="form-label">Category</label>
-        <select id="category" class="form-select" required>
+        <select id="category" class="form-select" required  v-model="selected">
           <option disabled value="">Select Category</option>
           <option value="Food">Food</option>
           <option value="Transport">Transport</option>
@@ -34,6 +49,7 @@
         <label for="date" class="form-label">Date</label>
         <input
           type="date"
+          v-model="date"
           id="date"
           class="form-control"
           required
@@ -46,6 +62,7 @@
         <label for="description" class="form-label">Description (Optional)</label>
         <textarea
           id="description"
+          v-model="description"
           class="form-control"
           rows="3"
           placeholder="Enter description"
